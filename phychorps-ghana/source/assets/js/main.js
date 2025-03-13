272e449008d8cf11aefb51e07a3d1f11e20bd908
1253William <williamofosuparwar@gmsil.com>
2025-03-13 10:44:06 +0000

    source updated

--- phychorps-ghana/source/assets/js/main.js
+++ phychorps-ghana/source/assets/js/main.js
@@ -279,41 +279,7 @@
         });
 
 
-        /* ==================================================
-            Contact Form Validations
-        ================================================== */
-        $('.contact-form').each(function() {
-            var formInstance = $(this);
-            formInstance.submit(function() {
-
-                var action = $(this).attr('action');
-
-                $("#message").slideUp(750, function() {
-                    $('#message').hide();
-
-                    $('#submit')
-                        .after('<img src="assets/img/ajax-loader.gif" class="loader" />')
-                        .attr('disabled', 'disabled');
-
-                    $.post(action, {
-                            name: $('#name').val(),
-                            email: $('#email').val(),
-                            phone: $('#phone').val(),
-                            comments: $('#comments').val()
-                        },
-                        function(data) {
-                            document.getElementById('message').innerHTML = data;
-                            $('#message').slideDown('slow');
-                            $('.contact-form img.loader').fadeOut('slow', function() {
-                                $(this).remove()
-                            });
-                            $('#submit').removeAttr('disabled');
-                        }
-                    );
-                });
-                return false;
-            });
-        });
+        
 
     }); // end document ready function
 })(jQuery); // End jQuery
